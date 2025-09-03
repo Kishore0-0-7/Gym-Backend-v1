5162473b40900e1033197b2061e584b67c642a36
Kishore0-0-7 <mrmks46007@gmail.com>
2025-09-03 12:49:28 +0530

    upadate

--- src/app.js
+++ src/app.js
@@ -7,18 +7,17 @@ const adminRoutes = require("./routes/admin.routes.js");
 const app = express();
 
 // ✅ Allowed origins
-const allowedOrigins = "*"
+const allowedOrigins = [
+  "http://localhost:5173", // for local dev
+  "https://gym.artechnology.pro", // main frontend domain (prod)
+  "https://gym-backend.artechnology.pro", // optional (API domain itself)
+];
 
 // ✅ Secure CORS config
 app.use(
   cors({
     origin: function (origin, callback) {
-      // Allow requests with no origin (like curl or Postman)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
+      
     },
     credentials: true, // allow cookies & auth headers
   })
