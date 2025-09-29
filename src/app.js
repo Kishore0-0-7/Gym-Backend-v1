9488a24d2612837b28beee1c03f3400c74813160
Kishore0_0_7 <mrmks46007@gmail.com>
2025-09-29 22:30:38 +0530

    update

--- src/app.js
+++ src/app.js
@@ -8,14 +8,32 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: function (origin, callback) {
+      // Allow all origins
+      callback(null, true);
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cookie",
+      "X-Requested-With",
+      "Accept",
+      "Origin",
+    ],
+    exposedHeaders: ["Set-Cookie"],
+    optionsSuccessStatus: 200,
   })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set("trust proxy", 1);
+
+// Handle preflight OPTIONS requests for all routes
+app.options("*", cors());
+
 app.use("/api/admin", adminRoutes);
 
 module.exports = app;
