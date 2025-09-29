11996390a746d51e8e1300545b3279eca30a5baf
Kishore0_0_7 <mrmks46007@gmail.com>
2025-09-29 12:48:59 +0530

    update

--- src/controller/adminPost.controller.js
+++ src/controller/adminPost.controller.js
@@ -282,7 +282,7 @@ const returnToken = async (req, res) => {
     const token = jwt.sign(payload, JWT_SECRET, { expiresIn });
     const maxAge = isRemember ? 24 * 60 * 60 * 1000 : 60 * 60 * 1000;
 
-    const isProduction = false; // change to true when deploying
+    const isProduction = true; // change to true when deploying
 
     res.cookie("token", token, {
       httpOnly: true,
